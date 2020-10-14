9ef17a85c3a04c8fef6d6abff7f08c82f0ec2e51
hlystovea <72656849+hlystovea@users.noreply.github.com>
2020-10-14 22:33:25 +0700

    Update service.py

--- service.py
+++ service.py
@@ -23,7 +23,7 @@ def weather():
 	# координаты мест для вывода погоды
 	places = {
     	'name': ['Гладенькая', 'Ергаки', 'Черемушки'],
-		'lat': ['52.917383', '52.837717', '52.853902'],
+	'lat': ['52.917383', '52.837717', '52.853902'],
     	'lon': ['91.352284', '93.255870', '91.408986'],
     }
 
@@ -52,8 +52,8 @@ def weather():
 		'thunderstorm-with-hail': 'гроза с градом',
 	}
 
-	list_weather_ya = f'По данным ["Яндекс.Погода"](https://clck.ru/RMoit) сейчас:\n'
-	list_weather_op = f'По данным "OpenWeather" сейчас:\n'
+	list_weather_ya = f'По данным ["Яндекс.Погода"](https://yandex.ru/pogoda/cheryomushki) сейчас:\n'
+	list_weather_op = f'По данным ["OpenWeather"](https://openweathermap.org/city/1492893) сейчас:\n'
 
 	response_ya = requests.get(url_ya, headers=header_ya)
 	response_op = requests.get(url_op, params=param_op)
@@ -78,7 +78,7 @@ def weather():
 				sn = ''
 			
 			place = places['name'][i]
-			fact_weather = f'*{place}*: {sn}{fact_temp} \xb0С, {fact_wind} м/с, {condition[fact_cond]}.\n'
+			fact_weather = f'{place}: {sn}{fact_temp} \xb0С, {fact_wind} м/с, {condition[fact_cond]}.\n'
 			list_weather_ya = list_weather_ya + fact_weather
 		return list_weather_ya
 	elif response_op.status_code == 200: # проверяет доступность сервера погоды
