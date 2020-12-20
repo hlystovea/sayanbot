6d16a669dca26479e42683383959b77c9949bc0b
Evgeniy Khlystov <hlystovea@gmail.com>
2020-12-20 15:06:05 +0700

    Added gismeteo

--- service.py
+++ service.py
@@ -8,7 +8,7 @@ def weather():
 	open_token = os.environ.get('OP_TOKEN')
 	gis_token = os.environ.get('GIS_TOKEN')
 
-	url_ya = 'https://api.weather.yandex.ru/v2/forecast/'
+	url_ya = 'https://api.weather.yandex.ru/v2/informers/'
 	url_op = 'https://api.openweathermap.org/data/2.5/weather/'
 	url_gis = 'https://api.gismeteo.net/v2/weather/current/'
 
@@ -35,16 +35,22 @@ def weather():
 		'longitude': '91.351843',
 	}
 
-	# координаты мест для вывода погоды
+	param_ya = {
+		'lang': 'ru-RU',
+		'lat': '52.916296',
+		'lon': '91.351843',
+	}
+
+# координаты мест для вывода погоды
 	places = {
-		'name': ['Гладенькая', 'Ергаки', 'Черемушки', 'Приисковый'],
-		'lat': ['52.917383', '52.837717', '52.853902', '54.652993'],
-		'lon': ['91.352284', '93.255870', '91.408986', '88.703671'],
+		'name': ['Гладенькая', 'Ергаки', 'Черемушки', 'Приисковый', 'Сочи'],
+		'lat': ['52.917383', '52.837717', '52.853902', '54.652993', '43.585472'],
+		'lon': ['91.352284', '93.255870', '91.408986', '88.703671',  '39.723089'],
 	}
 
 	quan = len(places['name']) # определяет количество запросов о погоде
 
-	# расшифровка осадков для яндекс погоды
+# расшифровка осадков для яндекс погоды
 	condition = {
 		'clear': 'ясно',
 		'partly-cloudy': 'малооблачно',
@@ -67,24 +73,15 @@ def weather():
 		'thunderstorm-with-hail': 'гроза с градом',
 	}
 
-	# расшифровка состояния неба для gismeteo
-	cloudiness = {
-		0: 'ясно',
-		1: 'малооблачно',
-		2: 'облачно',
-		3: 'пасмурно',
-		101: 'переменная облачность',
-	}
-
 	list_weather_ya = 'По данным ["Яндекс.Погода"](https://yandex.ru/) сейчас:\n'
 	list_weather_op = 'По данным ["OpenWeather"](https://openweathermap.org/) сейчас:\n'
 	list_weather_gis = 'По данным ["Gismeteo"](https://www.gismeteo.ru/) сейчас:\n'
 
-	response_ya = requests.get(url_ya, headers=header_ya)
+	response_ya = requests.get(url_ya, headers=header_ya, params=param_ya)
 	response_op = requests.get(url_op, params=param_op)
 	response_gis = requests.get(url_gis, params=param_gis, headers=head_gis)
 
-	if response_gis.status_code == 200:
+	if response_gis.status_code == 200: # проверяет доступность сервера погоды
 		for i in range(0, quan): # перебирает места и формирует список погоды
 			param_gis = {
 				'lang': 'ru',
@@ -96,15 +93,11 @@ def weather():
 			weather = json.loads(response.text)
 
 			fact_temp = weather['response']['temperature']['air']['C']
-			fact_cond = weather['response']['cloudiness']['type']
+			fact_cond = weather['response']['description']['full'].lower()
 			fact_wind = weather['response']['wind']['speed']['m_s']
 
-			sn = '+'
-			if fact_temp <= 0: # знак перед значением температуры
-				sn = ''
-
 			place = places['name'][i]
-			fact_weather = f'{place}: {sn}{fact_temp} \xb0С, {fact_wind} м/с, {cloudiness[fact_cond]}.\n'
+			fact_weather = f'{place}: {fact_temp:+.1f} \xb0С, {fact_wind} м/с, {fact_cond}.\n'
 			list_weather_gis = list_weather_gis + fact_weather
 		return list_weather_gis
 	elif response_ya.status_code == 200: # проверяет доступность сервера погоды
@@ -123,12 +116,8 @@ def weather():
 			fact_cond = weather['fact']['condition']
 			fact_wind = weather['fact']['wind_speed']
 
-			sn = '+'
-			if fact_temp <= 0: # знак перед значением температуры
-				sn = ''
-
 			place = places['name'][i]
-			fact_weather = f'{place}: {sn}{fact_temp} \xb0С, {fact_wind} м/с, {condition[fact_cond]}.\n'
+			fact_weather = f'{place}: {fact_temp:+.1f} \xb0С, {fact_wind} м/с, {condition[fact_cond]}.\n'
 			list_weather_ya = list_weather_ya + fact_weather
 		return list_weather_ya
 	elif response_op.status_code == 200: # проверяет доступность сервера погоды
@@ -146,13 +135,13 @@ def weather():
 			fact_cond = weather['weather'][0]['description']
 			fact_wind = weather['wind']['speed']
 
-			sn = '+'
-			if fact_temp <= 0: # знак перед значением температуры
-				sn = ''
-
 			place = places['name'][i]
-			fact_weather = f'{place}: {sn}{fact_temp} \xb0С, {fact_wind} м/с, {fact_cond}.\n'
+			fact_weather = f'{place}: {fact_temp:+.1f} \xb0С, {fact_wind} м/с, {fact_cond}.\n'
 			list_weather_op = list_weather_op + fact_weather
 		return list_weather_op
 	else:
 		return f'Проблемы со связью. Попробуйте немного позже.'
+
+
+if __name__ == '__main__':
+	print(weather())
